38967a3924c92803993025d9016c638662c424f7
zychen <czyling1125@163.com>
2018-10-04 19:44:47 +0800

    first commit

---
+++ app/src/main/java/cn/edu/pku/zy/miniweather/MainActivity.java
@@ -0,0 +1,12 @@
+package cn.edu.pku.zy.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
